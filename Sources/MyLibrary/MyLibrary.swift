261f8d3262e29fa4f09e0cdb0d75c6c6a1474f11
Anand Kumar <anand.kumar2@comviva.com>
2022-04-05 15:55:33 +0530

    Initial Commit

---
+++ Sources/MyLibrary/MyLibrary.swift
@@ -0,0 +1,10 @@
+public struct MyLibrary {
+    public private(set) var text = "Hello, World!"
+
+    public init() {
+    }
+    
+    public func HellowToYou() -> String {
+        return "Hello To You! "
+    }
+}
